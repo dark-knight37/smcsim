d3d076db7890ef9c3cfa17b96106c15d0b466407
Stefano Marrone <stefano.marrone@unicampania.it>
2020-06-02 22:50:33 +0200

    Now it is possible to load from a configuration file

--- core/blackboard.py
+++ core/blackboard.py
@@ -1,18 +1,99 @@
 import configparser
 from core.metaclasses import Singleton
 
-class BlackboardUtils():
+class BlackboardFactory():
     @staticmethod
-    def yearsToSec(yrs):
-        return BlackboardUtils.daysToSec(365 * yrs)
+    def years(yrs):
+        temp = 365 * float(yrs)
+        temp = BlackboardFactory.days(temp)
+        return temp
 
     @staticmethod
-    def daysToSec(dys):
-        return BlackboardUtils.hoursToSec(24 * dys)
+    def days(dys):
+        temp = 24 * float(dys)
+        temp = BlackboardFactory.hours(temp)
+        return temp
 
     @staticmethod
-    def hoursToSec(hrs):
-        return hrs * 3600
+    def hours(hrs):
+        temp = float(hrs) * 3600
+        return temp
+
+    @staticmethod
+    def load(inifile):
+        black = Blackboard()
+        reader = configparser.ConfigParser()
+        reader.read(inifile)
+        try:
+            temp = reader['main']['experiments']
+            black.put('experiments',int(temp))
+            temp = reader['main']['parallelism']
+            black.put('parallelism',int(temp))
+            temp = reader['main']['debugLevel']
+            black.put('debugLevel',int(temp))
+            temp = reader['main']['kindmetric']
+            black.put('kindmetric',temp)
+            temp = reader['main']['stoptime']
+            temp = BlackboardFactory.tosecond(temp)
+            black.put('stoptime',temp)
+            sections = BlackboardFactory.tolist(reader['main']['sections'])
+            for s in sections:
+                temp = BlackboardFactory.loadSection(reader,s)
+                Blackboard().merge(temp)
+        except Exception as s:
+            print(s)
+
+
+    @staticmethod
+    def tosecond(value):
+        mapping = {
+            'years': BlackboardFactory.years,
+            'days': BlackboardFactory.days,
+            'hours': BlackboardFactory.hours,
+            'int': int,
+            'float': float,
+            'bool': bool
+        }
+        vals = value.split(',')
+        f = mapping[vals[1]]
+        retval = f(vals[0])
+        return retval
+
+    @staticmethod
+    def checktime(value):
+        acceptable = ['years','days','hours','int','float']
+        vals = value.split(',')
+        found = False
+        if len(vals) == 2:
+            try:
+                found = vals[1] in acceptable
+            except TypeError:
+                found = False
+        return found
+
+
+    @staticmethod
+    def tolist(value):
+        retval = value.split(',')
+        return retval
+
+    @staticmethod
+    def loadSection(reader,s):
+        temp = dict()
+        options = reader.options(s)
+        for o in options:
+            try:
+                value = reader[s][o]
+                if (BlackboardFactory.checktime(value)):
+                    value = BlackboardFactory.tosecond(value)
+                temp['[' + s + ']' + o] = value
+            except:
+                print("exception on %s!" % o)
+                temp[o] = None
+        return temp
+
+
+
 
 
 class Blackboard(metaclass=Singleton):
@@ -26,51 +107,5 @@ class Blackboard(metaclass=Singleton):
     def put(self,key,value):
         self.board[key] = value
 
-    @staticmethod
-    def load(inifile):
-        black = Blackboard()
-        reader = configparser.ConfigParser()
-        reader.read(inifile)
-        opts = reader.options('[main]')
-        for o in opts:
-            try:
-                temp = reader.get('[main]',o)
-                black.put(o,temp)
-            except:
-                print("exception on %s!" % o)
-
-                import ConfigParser
-
-                class AbstractFactory():
-                    def __init__(self, inifile, kind=''):
-                        self.reader = ConfigParser.ConfigParser()
-                        self.reader.read(inifile)
-
-                    def genList(self, sectName, f, size):
-                        td = self.getSection(sectName)
-                        retval = map(lambda i: f(td, i), range(0, size))
-                        return retval
-
-                    def getSection(self, s):
-                        temp = dict()
-                        options = self.reader.options(s)
-                        for o in options:
-                            try:
-                                temp[o] = self.reader.get(s, o)
-                                if temp[o] == -1:
-                                    print("skip: %s" % o)
-                            except:
-                                print("exception on %s!" % o)
-                                temp[o] = None
-                        return temp
-
-                    def generate(self, kind=""):
-                        pass
-
-                    def listFormatting(self, s):
-                        if (s == '-'):
-                            retval = None
-                        else:
-                            retval = s[1:-1]
-                            retval = retval.split(',')
-                        return retval
+    def merge(self,dic):
+        self.board.update(dic)
