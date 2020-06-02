dd94e1a2b67cc0aea9a578cdc23dc092683a481c
stefanomarrone <stefano.marrone@unicampania.it>
2020-06-02 17:37:20 +0200

    temp update

--- core/blackboard.py
+++ core/blackboard.py
@@ -1,6 +1,20 @@
 import configparser
 from core.metaclasses import Singleton
 
+class BlackboardUtils():
+    @staticmethod
+    def yearsToSec(yrs):
+        return BlackboardUtils.daysToSec(365 * yrs)
+
+    @staticmethod
+    def daysToSec(dys):
+        return BlackboardUtils.hoursToSec(24 * dys)
+
+    @staticmethod
+    def hoursToSec(hrs):
+        return hrs * 3600
+
+
 class Blackboard(metaclass=Singleton):
 
     def __init__(self):
@@ -24,3 +38,39 @@ class Blackboard(metaclass=Singleton):
                 black.put(o,temp)
             except:
                 print("exception on %s!" % o)
+
+                import ConfigParser
+
+                class AbstractFactory():
+                    def __init__(self, inifile, kind=''):
+                        self.reader = ConfigParser.ConfigParser()
+                        self.reader.read(inifile)
+
+                    def genList(self, sectName, f, size):
+                        td = self.getSection(sectName)
+                        retval = map(lambda i: f(td, i), range(0, size))
+                        return retval
+
+                    def getSection(self, s):
+                        temp = dict()
+                        options = self.reader.options(s)
+                        for o in options:
+                            try:
+                                temp[o] = self.reader.get(s, o)
+                                if temp[o] == -1:
+                                    print("skip: %s" % o)
+                            except:
+                                print("exception on %s!" % o)
+                                temp[o] = None
+                        return temp
+
+                    def generate(self, kind=""):
+                        pass
+
+                    def listFormatting(self, s):
+                        if (s == '-'):
+                            retval = None
+                        else:
+                            retval = s[1:-1]
+                            retval = retval.split(',')
+                        return retval
