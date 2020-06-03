a7109b02d07490451c67c469c666c17114785bd9
jamespotter24 <cristinawise208@gmail.com>
2020-06-03 21:12:11 +0200

    working revision

--- core/measures.py
+++ core/measures.py
@@ -1,9 +1,39 @@
-from core.blackboard import Blackboard
 
-class Measure():
+class Sample():
+    def __init__(self,hheader):
+        self.header = hheader
+        self.lst = list()
+
+    def add(self,sample):
+        self.lst.append(sample)
+
+    def tocsv(self):
+        retval = self.header + '\n'
+        for i in self.lst:
+            retval += i.tocsv() + '\n'
+        return retval
+
+
+
+class Measurement():
+    def __init__(self):
+        self.samples = dict()
 
     def fitness(self):
         pass
 
-    def get(self):
-        return Blackboard().get('[Measure]')
+    def getKeys(self,tags):
+        retval = self.samples.keys() if (tags is None) else list(tags)
+        return retval
+
+    def mean(self,tags = None):
+        keys = self.getKeys(tags)
+        retval = dict()
+        for k in keys:
+
+
+
+
+
+    def csv(self):
+        pass
