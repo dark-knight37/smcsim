87087615d9ee39688cbb6d8c3bc5ff4492146026
Stefano Marrone <stefano.marrone@unicampania.it>
2020-06-04 15:51:25 +0200

    A first candidate revision

--- batch_robot.py
+++ batch_robot.py
@@ -1,14 +1,25 @@
 from test_robot import root
+import subprocess
+import multiprocessing
 import sys
 import os
+
+
+def core(inifile,logfile):
+    print('starting')
+    list_files = subprocess.call(["python3", "test_robot.py", inifile, logfile])
+    print('ending')
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         directory = sys.argv[1]
         filenames = filter(lambda x: x.endswith('.ini'), os.listdir(directory))
+        jobs = list()
         for fn in filenames:
-            print(fn)
-            log = fn[:-4] + '.log'
-            args = [None, directory + '/' + fn, directory + '/' + log]
-            root(args)
-
+            ln = directory + fn[:-4] + '.log'
+            fn = directory + fn
+            p = multiprocessing.Process(target=core, args=(fn,ln))
+            jobs.append(p)
+            p.start()
